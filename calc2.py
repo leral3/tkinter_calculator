6553486b69c8eea2e914f132523461fc375150fd
leral3 <leral3@naver.com>
2022-03-08 22:07:29 +0900

    화면 초기화

---
+++ calc2.py
@@ -0,0 +1,17 @@
+from tkinter import *  # import tkinter 과의 차이는 사용법상으로 모듈 이름을 계속 붙여주느냐 아니냐의 차이입니다. 
+                       # 만약에 import 모듈 했으면 모듈.매서드() 이런식으로 계속 작성해야하지만, from 모듈 import * 이렇게라면 매서드() 이렇게만 써주면 된다. 
+from tkinter import ttk  # 기존 Tk() 클래스보다 그래픽이 개선 되었다. 
+
+
+# 함수를 추가할 부분 
+
+
+root = Tk()
+root.title("Calculator")
+root.geometry("200x200")
+
+
+# 인터페이스 (버튼, 창) 추가할 부분
+
+
+root.mainloop()
